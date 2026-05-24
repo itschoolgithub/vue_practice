53c03bc851135ae21b444c975dc3300cfdeff783
itschool <itschoolgithub@mail.ru>
2026-05-24 13:25:00 +0500

    comments

--- practice2.js
+++ practice2.js
@@ -13,63 +13,86 @@ const config = {
     },
     methods: {
         startGame(hardMode) {
+            // начать игру
+            // сбрасываем все переменные
             this.hardMode = hardMode;
             this.gamePlaying = true;
             this.leftTime = 30;
             this.scores = 0;
             this.prevCell = null;
             this.clearField();
+
+            // запускам первый тик
             setTimeout(this.tick, 100);
         },
         restartGame() {
+            // начать игру заново
             this.gameFinished = false;
             this.startGame();
         },
         clearField() {
+            // очистка поля
             this.cells = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         },
         tick() {
+            // увеличиваем тик на 1
             this.tickNumber++;
+
+            if (this.tickNumber == 5) {
+                if (this.hardMode) {
+                    // если прошло 5 тиков и это тяжелый режим
+                    // спавним моба
+                    this.spaunMob();
+                }
+            }
+
+            if (this.tickNumber >= 10) {
+                // если прошло 10 тиков (т.е. секунда)
+                this.leftTime--;
+                this.tickNumber = 0;
+                this.spaunMob();
+            }
+
             if (this.leftTime <= 0) {
+                // если время закончилось
                 this.gamePlaying = false;
                 this.gameFinished = true;
                 return;
             }
-            if (this.tickNumber == 5) {
-                if (this.hardMode) {
-                    this.spaunEnemy();
-                }
-            }
-            if (this.tickNumber >= 10) {
-                this.leftTime--;
-                this.tickNumber = 0;
-                this.spaunEnemy();
-            }
             
+            // снова вызываем Тик
             setTimeout(this.tick, 100);
         },
-        spaunEnemy() {
+        spaunMob() {
+            // очищаем поле
             this.clearField();
-            let index = Math.round(Math.random()*8);
+            // получаем рандомную клетку
+            let index = Math.floor(Math.random()*8);
             if (this.prevCell == index) {
-                this.spaunEnemy();
+                // если выпала та же клетка, что и в прошлый раз
+                this.spaunMob();
                 return;
             }
             this.prevCell = index;
-            this.cells[index] = this.getTypeEnemy();
+            // устанавливаем в клетку персонажа
+            this.cells[index] = this.getTypeMob();
         },
-        getTypeEnemy() {
-            let chance = Math.round(Math.random()*3)
-            if (chance == 3) {
+        getTypeMob() {
+            let chance = Math.floor(Math.random()*2)
+            // 1 к 3, что выпадет енот
+            if (chance == 2) {
                 return 3;
             }
+            // иначе нло
             return 1;
         },
         onClickCell(index) {
             if (this.cells[index] == 1) {
+                // если это нло
                 this.scores++;
                 this.cells[index]++;
             } else if (this.cells[index] == 3) {
+                // если это енот
                 this.scores--;
                 this.cells[index]++;
             }
